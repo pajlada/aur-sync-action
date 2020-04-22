04c85c47a02321945d326ae9dcd1823b5964f814
maniacata <maniaciachao@gmail.com>
2020-04-22 11:42:21 +0800

    :bug: (dockerfile) install extra denpendencies in runtime

--- Dockerfile
+++ Dockerfile
@@ -4,22 +4,19 @@ RUN pacman -Sy && \
     pacman -Sy --noconfirm openssh \
     git fakeroot binutils go-pie   \
     gcc awk binutils coreutils  \
-    file gettext grep jq perl \
-    $INPUT_EXTRA_DEPENDENCIES
+    file gettext grep jq
 
-RUN useradd -ms /bin/bash builder && \
-    mkdir -p /home/builder/.ssh && \
-    touch /home/builder/.ssh/known_hosts
+RUN mkdir -p /root/.ssh && \
+    touch /root/.ssh/known_hosts
 
-COPY ssh_config /home/builder/.ssh/config
+COPY ssh_config /root/.ssh/config
 
-RUN chown builder:builder /home/builder -R && \
-    chmod 600 /home/builder/.ssh/* -R
+RUN chmod 600 /root/.ssh/* -R
 
 COPY entrypoint.sh /entrypoint.sh
 
-USER builder
-WORKDIR /home/builder
+# USER root
+WORKDIR /root
 
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,17 @@ COMMIT_EMAIL=$INPUT_COMMIT_EMAIL
 SSH_PRIVATE_KEY=$INPUT_SSH_PRIVATE_KEY
 GITHUB_REPO=$INPUT_GITHUB_REPO
 
-export HOME=/home/builder
-
+# config ssh 
 ssh-keyscan -t ed25519 aur.archlinux.org >> $HOME/.ssh/known_hosts
-
 echo -e "${SSH_PRIVATE_KEY//_/\\n}" > $HOME/.ssh/aur
-
 chmod 600 $HOME/.ssh/aur*
 
+# config git
 git config --global user.name "$COMMIT_USERNAME"
 git config --global user.email "$COMMIT_EMAIL"
-
 AUR_REPO_URL="ssh://aur@aur.archlinux.org/${PACKAGE_NAME}.git"
 
-echo "---------------- $AUR_REPO_URL ----------------"
-
+echo "---------------- CLONE REPO ----------------"
 cd /tmp
 git clone "$AUR_REPO_URL"
 cd "$PACKAGE_NAME"
@@ -33,32 +29,34 @@ RELEASE_VER=`curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -
 NEW_PKGVER="${RELEASE_VER:1}" # remove character 'v'
 CURRENT_VER=`grep pkgver .SRCINFO | awk -F '=' '{print $2}' | tr -d "[:space:]"`
 
-# diff version
 echo "release version is "$NEW_PKGVER
 echo "current version is "$CURRENT_VER
+
 if [[ $NEW_PKGVER = $CURRENT_VER ]]; then
   echo "already up-to-date!";
   echo "------------- SYNC DONE ----------------"
   exit 0
 fi
 
-
 echo "------------- BUILDING PKG $PACKAGE_NAME ----------------"
 
+if [[ ! -z "$INPUT_EXTRA_DEPENDENCIES" ]]; then
+  echo "------------- EXTRA DENPENDENCIES ----------------"
+  pacman pacman -Sy --noconfirm $EXTRA_DEPENDENCIES
+fi
+
+echo "------------- MAKE PACKAGE ----------------"
 sed -i "s/pkgver=.*$/pkgver=${NEW_PKGVER}/" PKGBUILD
 sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
 perl -i -0pe "s/sha256sums=[\s\S][^\)]*\)/$(makepkg -g 2>/dev/null)/" PKGBUILD
-
-# Test build
+# test build
 makepkg -c
-
-# Update srcinfo
+# update srcinfo
 makepkg --printsrcinfo > .SRCINFO
-
 
 echo "------------- BUILD DONE ----------------"
 
-# Update aur
+# update aur
 git add PKGBUILD .SRCINFO
 git commit --allow-empty  -m "Update to $NEW_RELEASE"
 git push
